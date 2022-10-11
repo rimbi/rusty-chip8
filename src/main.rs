5a347040a6970840825d26290724433f81a4738d
Cem Eliguzel <ce@concordium.com>
2022-10-11 20:09:31 +0300

    Implements several instructions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::Read,
     path::{Path, PathBuf},
+    time::{Duration, Instant},
     vec,
 };
 
@@ -14,21 +16,57 @@ const TERMINAL_WIDTH: u8 = 64;
 const TERMINAL_HEIGHT: u8 = 32;
 const RAM_SIZE: usize = 4096;
 
-#[derive(Debug, StructOpt)]
-struct Opt {
-    #[structopt(name = "ROM_FILE_PATH", parse(from_os_str))]
-    rom: PathBuf,
-}
+const FONT_ADDR: usize = 0x50;
+const FONT_SIZE: usize = 5;
+const FONTS: [u8; 80] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
 
 /// Chip8 emulator state.
-#[derive(Debug, Default)]
+#[derive(Debug)]
 struct Chip8 {
     pixels: Vec<Vec<bool>>,
     ram: Vec<u8>,
     pc: usize,
-    i: u16,
-    // stack: Vec<u16>,
+    i: usize,
+    stack: Vec<usize>,
     registers: [u8; 16],
+    delay_timer: u8,
+    sound_timer: u8,
+    last_time: Instant,
+    key_pressed: Option<u8>,
+}
+
+impl Default for Chip8 {
+    fn default() -> Self {
+        Self {
+            pixels: Default::default(),
+            ram: Default::default(),
+            pc: Default::default(),
+            i: Default::default(),
+            stack: Default::default(),
+            registers: Default::default(),
+            delay_timer: Default::default(),
+            sound_timer: Default::default(),
+            last_time: Instant::now(),
+            key_pressed: Default::default(),
+        }
+    }
 }
 
 /// Represents Chip8 instructions.
@@ -40,6 +78,24 @@ enum Instruction {
     Dxyn(u8, u8, u8),
     Add(u8, u8),
     Jump(u16),
+    SubroutineCall(u16),
+    SubroutineReturn,
+    SkipEqual(u8, u8),
+    SkipNotEqual(u8, u8),
+    BinaryCodedDecimalConversion(u8),
+    LoadRegisters(u8),
+    FontCharacter(u8),
+    SetDelayTimer(u8),
+    ReadDelayTimer(u8),
+    SetSoundTimer(u8),
+    Random(u8, u8),          // CXNN
+    SkipIfKeyPressed(u8),    // EX9E
+    SkipIfKeyNotPressed(u8), // EXA1
+    BinaryAnd(u8, u8),       // 8XY2
+    RegisterAdd(u8, u8),     // 8XY4
+    RegisterSet(u8, u8),     // 8XY0
+    RegisterSub(u8, u8),     // 8XY5
+    RegisterSubRev(u8, u8),  // 8XY7
 }
 
 impl Instruction {
@@ -57,19 +113,57 @@ impl Instruction {
             (6, x, _, _, nn, _) => Instruction::SetVRegister(x, nn),
             (0xD, x, y, n, _, _) => Instruction::Dxyn(x, y, n),
             (7, x, _, _, nn, _) => Instruction::Add(x, nn),
-            _ => bail!("unimplemented instruction: {} {} {} {}", i, x, y, n),
+            (2, _, _, _, _, nnn) => Instruction::SubroutineCall(nnn),
+            (0, 0, 0xE, 0xE, _, _) => Instruction::SubroutineReturn,
+            (3, x, _, _, nn, _) => Instruction::SkipEqual(x, nn),
+            (4, x, _, _, nn, _) => Instruction::SkipNotEqual(x, nn),
+            (0xF, x, 3, 3, _, _) => Instruction::BinaryCodedDecimalConversion(x),
+            (0xF, x, 6, 5, _, _) => Instruction::LoadRegisters(x),
+            (0xF, x, 2, 9, _, _) => Instruction::FontCharacter(x),
+            (0xF, x, 1, 5, _, _) => Instruction::SetDelayTimer(x),
+            (0xF, x, 0, 7, _, _) => Instruction::ReadDelayTimer(x),
+            (0xF, x, 1, 8, _, _) => Instruction::SetSoundTimer(x),
+            (0xC, x, _, _, nn, _) => Instruction::Random(x, nn),
+            (0xE, x, 9, 0xE, _, _) => Instruction::SkipIfKeyPressed(x),
+            (0xE, x, 0xA, 1, _, _) => Instruction::SkipIfKeyNotPressed(x),
+            (8, x, y, 2, _, _) => Instruction::BinaryAnd(x, y),
+            (8, x, y, 4, _, _) => Instruction::RegisterAdd(x, y),
+            (8, x, y, 0, _, _) => Instruction::RegisterSet(x, y),
+            (8, x, y, 5, _, _) => Instruction::RegisterSub(x, y),
+            (8, x, y, 7, _, _) => Instruction::RegisterSubRev(x, y),
+            _ => {
+                std::thread::sleep(Duration::from_secs(5));
+                bail!("unimplemented instruction: {} {} {} {}", i, x, y, n)
+            }
         };
         Ok(ins)
+    }
+
+    pub fn requires_pc_inc(&self) -> usize {
+        match self {
+            Self::SubroutineCall(_) => 0,
+            Self::SubroutineReturn => 4,
+            Self::Jump(_) => 0,
+            _ => 2,
+        }
     }
 }
 
 impl GameState for Chip8 {
     fn tick(&mut self, ctx: &mut bracket_terminal::prelude::BTerm) {
+        if self.last_time.elapsed().as_millis() > (1000. / 60.) as u128 {
+            self.handle_keys(ctx);
+            self.last_time = Instant::now();
+            self.delay_timer = self.delay_timer.saturating_sub(1);
+            self.sound_timer = self.sound_timer.saturating_sub(1);
+        }
         let inst = self
             .fetch_and_decode_next_instruction()
             .expect("instruction failure");
+        // println!("oc = {}, inst = {:?}", self.pc, inst);
         self.execute_instruction(inst, ctx)
             .unwrap_or_else(|_| panic!("failed to execute instruction: {:?}", inst));
+        self.pc += inst.requires_pc_inc();
     }
 }
 
@@ -85,7 +179,6 @@ impl Chip8 {
             .get(self.pc + 1)
             .unwrap_or_else(|| panic!("invalid memory address: {}", self.pc));
         let inst = Instruction::new(b1, b2).context("failed to decode instruction")?;
-        self.pc += 2;
         Ok(inst)
     }
 
@@ -93,7 +186,7 @@ impl Chip8 {
     fn execute_instruction(&mut self, inst: Instruction, ctx: &mut BTerm) -> Result<()> {
         match inst {
             Instruction::Cls => ctx.cls(),
-            Instruction::SetIndexRegister(nnn) => self.i = nnn,
+            Instruction::SetIndexRegister(nnn) => self.i = nnn as usize,
             Instruction::SetVRegister(x, nn) => self.registers[x as usize] = nn,
             Instruction::Dxyn(x, y, n) => {
                 let x_org = (self.registers[x as usize] % TERMINAL_WIDTH) as usize;
@@ -130,8 +223,133 @@ impl Chip8 {
                 self.registers[x as usize] = self.registers[x as usize].saturating_add(nn)
             }
             Instruction::Jump(nnn) => self.pc = nnn.into(),
+            Instruction::SubroutineCall(nnn) => {
+                self.stack.push(self.pc);
+                self.pc = nnn as usize;
+            }
+            Instruction::SubroutineReturn => {
+                self.pc = self
+                    .stack
+                    .pop()
+                    .context("failed to return from subroutine: stack underflow")?;
+            }
+            Instruction::SkipEqual(x, nn) => {
+                // println!("v{} = {}", x, self.registers[x as usize]);
+                if self.registers[x as usize] == nn {
+                    self.pc += 2;
+                }
+            }
+            Instruction::SkipNotEqual(x, nn) => {
+                if self.registers[x as usize] != nn {
+                    self.pc += 2;
+                }
+            }
+            Instruction::BinaryCodedDecimalConversion(x) => {
+                let val = self.registers[x as usize];
+                self.ram[self.i] = val / 100;
+                self.ram[self.i + 1] = (val % 100) / 10;
+                self.ram[self.i + 2] = val % 10;
+            }
+            Instruction::LoadRegisters(x) => {
+                self.registers[0..=x as usize]
+                    .copy_from_slice(&self.ram[self.i..=self.i + x as usize]);
+            }
+            Instruction::FontCharacter(x) => {
+                self.i = FONT_ADDR + (self.registers[x as usize] as usize * FONT_SIZE)
+            }
+            Instruction::SetDelayTimer(x) => self.delay_timer = self.registers[x as usize],
+            Instruction::ReadDelayTimer(x) => self.registers[x as usize] = self.delay_timer,
+            Instruction::SetSoundTimer(x) => self.sound_timer = self.registers[x as usize],
+            Instruction::Random(x, nn) => {
+                let r: u8 = rand::random();
+                self.registers[x as usize] = r & nn;
+            }
+            Instruction::SkipIfKeyPressed(x) => {
+                if self.key_pressed == Some(self.registers[x as usize]) {
+                    self.pc += 2;
+                }
+            }
+            Instruction::SkipIfKeyNotPressed(x) => {
+                if self.key_pressed != Some(self.registers[x as usize]) {
+                    self.pc += 2;
+                }
+            }
+            Instruction::BinaryAnd(x, y) => {
+                self.registers[x as usize] &= self.registers[y as usize]
+            }
+            Instruction::RegisterAdd(x, y) => {
+                let (res, carry) =
+                    self.registers[x as usize].overflowing_add(self.registers[y as usize]);
+                self.registers[x as usize] = res;
+                self.registers[15] = carry as u8;
+            }
+            Instruction::RegisterSet(x, y) => {
+                self.registers[x as usize] = self.registers[y as usize]
+            }
+            Instruction::RegisterSub(x, y) => {
+                let (res, carry) =
+                    self.registers[x as usize].overflowing_sub(self.registers[y as usize]);
+                self.registers[x as usize] = res;
+                self.registers[15] = !carry as u8;
+            }
+            Instruction::RegisterSubRev(x, y) => {
+                let (res, carry) =
+                    self.registers[y as usize].overflowing_sub(self.registers[x as usize]);
+                self.registers[x as usize] = res;
+                self.registers[15] = !carry as u8;
+            }
         }
         Ok(())
+    }
+
+    /// Handles and reacts to the key strokes by the player.
+    fn handle_keys(&mut self, ctx: &mut BTerm) {
+        let keymap = [
+            (VirtualKeyCode::Key1),
+            (VirtualKeyCode::Key2),
+            (VirtualKeyCode::Key3),
+            (VirtualKeyCode::Key4),
+            (VirtualKeyCode::Q),
+            (VirtualKeyCode::W),
+            (VirtualKeyCode::E),
+            (VirtualKeyCode::R),
+            (VirtualKeyCode::A),
+            (VirtualKeyCode::S),
+            (VirtualKeyCode::D),
+            (VirtualKeyCode::F),
+            (VirtualKeyCode::Z),
+            (VirtualKeyCode::X),
+            (VirtualKeyCode::C),
+            (VirtualKeyCode::V),
+        ]
+        .iter()
+        .zip(0u8..16)
+        .collect::<HashMap<_, _>>();
+        match ctx.key {
+            k @ Some(
+                VirtualKeyCode::Key1
+                | VirtualKeyCode::Key2
+                | VirtualKeyCode::Key3
+                | VirtualKeyCode::Key4
+                | VirtualKeyCode::Q
+                | VirtualKeyCode::W
+                | VirtualKeyCode::E
+                | VirtualKeyCode::R
+                | VirtualKeyCode::A
+                | VirtualKeyCode::S
+                | VirtualKeyCode::D
+                | VirtualKeyCode::F
+                | VirtualKeyCode::Z
+                | VirtualKeyCode::X
+                | VirtualKeyCode::C
+                | VirtualKeyCode::V,
+            ) => self.key_pressed = keymap.get(&k.unwrap()).cloned(),
+            None => self.key_pressed = None,
+            _ => {}
+        }
+        if self.key_pressed.is_some() {
+            println!("key pressed = {:?}", self.key_pressed);
+        }
     }
 
     /// Returns true if the pixel at the coordinates is on, otherwise false.
@@ -173,9 +391,11 @@ impl Chip8 {
     }
 
     pub fn new() -> Self {
+        let mut ram = vec![0; 512];
+        ram[FONT_ADDR..FONT_ADDR + FONTS.len()].copy_from_slice(&FONTS);
         Self {
             pixels: vec![vec![false; 64]; 32],
-            ram: vec![0; 512],
+            ram,
             pc: 512,
             ..Default::default()
         }
@@ -203,6 +423,14 @@ fn read_rom(path: &impl AsRef<Path>) -> Result<Vec<u8>> {
     Ok(data)
 }
 
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(name = "ROM_FILE_PATH", parse(from_os_str))]
+    rom: PathBuf,
+    #[structopt(short, long, default_value = "700")]
+    clock: u64,
+}
+
 fn main() -> BError {
     env_logger::init();
 
@@ -220,7 +448,7 @@ fn main() -> BError {
         .map_err(|_| anyhow::Error::msg("Failed to create the window"))?
         .with_title("Chip8")
         .with_tile_dimensions(14, 14)
-        .with_fps_cap(700.)
+        .with_fps_cap(opt.clock as f32)
         .build()?;
     main_loop(context, ch8)
 }
